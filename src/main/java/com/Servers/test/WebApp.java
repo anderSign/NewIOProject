5f564f16d51c005bd1b06970cc6e1d28d7d451f2
ander <1433188307@qq.com>
2020-12-25 17:06:31 +0800

    运行文件被修改

--- src/main/java/com/Servers/test/WebApp.java
+++ src/main/java/com/Servers/test/WebApp.java
@@ -11,6 +11,7 @@ import java.net.Socket;
  */
 public class WebApp {
     public static void main(String[] args) throws ClassNotFoundException {
+        System.out.println("修改后");
         WebAppHandler.openSocketServerInstance(5);
     }
 }
